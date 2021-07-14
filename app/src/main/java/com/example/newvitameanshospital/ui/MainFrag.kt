c3c7cc07f99f519fe8bbfda08e9578c6f3c9d8b4
zoozero99 <zzzkimjy@korea.ac.kr>
2021-07-14 10:49:23 +0900

    식이기록 ui 제작 선 추가

--- app/src/main/java/com/example/newvitameanshospital/ui/MainFrag.kt
+++ app/src/main/java/com/example/newvitameanshospital/ui/MainFrag.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.newvitameanshospital.R
 import com.example.newvitameanshospital.databinding.FragmentMainBinding
+import com.example.newvitameanshospital.ui.diet.DietFrag
 import com.example.newvitameanshospital.ui.myblood.BloodFrag
 import com.example.newvitameanshospital.ui.weight.WeightFragment
 
@@ -25,12 +26,14 @@ class MainFrag : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val dietFrag = DietFrag()
         val weightFrag = WeightFragment()
         val bloodFrag = BloodFrag()
         val manager = childFragmentManager
         val transaction = manager.beginTransaction()
         transaction.add(R.id.blood_pressure_sugar_frag,bloodFrag,bloodFrag.javaClass.name)
         transaction.add(R.id.weight_frag,weightFrag,weightFrag.javaClass.name)
+        transaction.add(R.id.dietary_record_frag,dietFrag,dietFrag.javaClass.name)
         transaction.commit()
 
     }
